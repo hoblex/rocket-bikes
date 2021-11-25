6d36912a67cbb45705bb9b32284d102efa23f2ff
Aleksandr Alekseev <hoblex@gmail.com>
2021-11-26 00:04:48 +0300

    add overlay

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,7 @@
 var pageHeader = document.querySelector('.page-header');
 var headerToggle = document.querySelector('.page-header__toggle');
 var pageBody = document.querySelector('.page-body');
+var overlay = document.querySelector('.page-body');
 
 pageHeader.classList.remove('page-header--nojs');
 pageHeader.classList.add('page-header--closed');
